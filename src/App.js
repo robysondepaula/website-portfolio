1f5fb3c465d37b3fb26fa56556d2407a0a59f11a
robysondepaula <robysonwillyandepaula@gmail.com>
2022-06-14 16:33:48 -0400

    Created About Section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import './App.css';
 import Cover from "./components/cover/Cover";
 import Navbar from './components/navbar/Navbar';
+import About from './components/about/About';
 
 function App() {
 const [scrollHeight, setScrollHeight] = useState(0);
@@ -19,6 +20,7 @@ useEffect(()=>{
     <div className="App">
       <Navbar isScrolling={scrollHeight} />
       <Cover />
+      <About />
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,23 @@
+
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div className="about-container">
+      <div className="about-desc">
+        <h3>About Me</h3>
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
+          aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis
+          suscipit eaque?
+        </p>
+      </div>
+      <div clasName="about-img">
+        <img src="https://images.pexels.com/photos/840996/pexels-photo-840996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="about"/>
+
+      </div>
+    </div>
+  );
+};
+export default About;
