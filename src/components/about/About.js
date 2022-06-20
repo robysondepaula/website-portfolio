36b58b0e0832ec1065f7c093da2e9aa3cf9ece1c
robysondepaula <robysonwillyandepaula@gmail.com>
2022-06-20 14:18:27 -0400

    Update about me

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import "./About.css";
 
@@ -8,14 +7,22 @@ const About = () => {
       <div className="about-desc">
         <h3>About Me</h3>
         <p>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Molestias
-          aut, repellat ipsum facere voluptate dicta obcaecati deserunt nobis
-          suscipit eaque?
+          My name is Robyson, I am a developer. I love technology, knowledge and
+          learning.
         </p>
+        <p>
+          I always aim to solve problems, write clean code, and deliver a unique
+          and fun experience to the user.
+        </p>
+        <br></br>
+        <h3>Languages and Frameworks</h3>
+        <ul>C#|JavaScript|HTML|CSS|TypeScript|React JS|ASP.NET|Bootstrap</ul>
       </div>
       <div className="about-img">
-        <img src="https://images.pexels.com/photos/840996/pexels-photo-840996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="about"/>
-
+        <img
+          src="https://images.pexels.com/photos/840996/pexels-photo-840996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          alt="about"
+        />
       </div>
     </div>
   );
